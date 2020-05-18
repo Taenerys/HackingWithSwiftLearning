9595c10eaf7bc459830f18928202b6466bd9517f
Taenerys <thuanh99@student.fdu.edu>
2020-05-18 00:45:34 -0700

    add project 3

--- Project3/DetailViewController.swift
+++ Project3/DetailViewController.swift
@@ -18,6 +18,8 @@ class DetailViewController: UIViewController {
         
         title = selectedTitle
         navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         // check the optional string selectedImage
         if let imageToLoad = selectedImage {
@@ -35,15 +37,15 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No Image Found Sorry")
+            return
+        }
+        
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
-    */
-
 }
